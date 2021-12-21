6eea5bb5294edfe6bfad324be986787f86e17e32
abdou70 <abdouniangyeumbeul7477>
2021-12-21 17:38:32 +0000

    first_commit_formBuilder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { CartComponent } from './cart/cart.component';
 import { FormglobComponent } from './formglob/formglob.component';
+import { FormulaireDynamiqueComponent } from './formulaire-dynamique/formulaire-dynamique.component';
 import { ProfilEditorComponent } from './profil-editor/profil-editor.component';
 
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   {path:'' , component : CartComponent},
   {path: 'profile' , component : ProfilEditorComponent},
   {path:'blog', component:BlogComponent},
-  {path:'formBlog',component:FormglobComponent}
+  {path:'formBlog',component:FormglobComponent},
+  {path: 'formDynam',component:FormulaireDynamiqueComponent}
 ];
 
 
--- src/app/formulaire-dynamique/formulaire-dynamique.component.ts
+++ src/app/formulaire-dynamique/formulaire-dynamique.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-dynamique',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulaire-dynamique.component.css']
 })
 export class FormulaireDynamiqueComponent implements OnInit {
-
+  infoForm:any;
+  montab:any =[];
+  //resp:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(){
+     this.infoForm = new FormGroup({
+      nom: new FormControl(),
+      prenom: new FormControl(),
+      adresse: new FormControl(),
+      telephone: new FormControl()
+    })
+  }
+
+  test(){
+    let tab1=this.infoForm.value;
+     this.montab=this.montab.push(tab1);
+    console.log("voici le nom : ",this.infoForm.get('nom').value);
+    console.log("voici le nom1 : ",tab1['nom']);
+    console.log("les valeurs que j'ai ajouter dans le tableau :",this.montab['nom']);
   }
 
 }
