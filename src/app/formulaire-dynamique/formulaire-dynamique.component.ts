9ea71baa329d44be924de9c77c84948075655784
abdou70 <abdouniangyeumbeul7477>
2021-12-22 16:38:54 +0000

    first_commit_features/controleBuilder

--- src/app/formulaire-dynamique/formulaire-dynamique.component.ts
+++ src/app/formulaire-dynamique/formulaire-dynamique.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire-dynamique',
@@ -9,28 +9,42 @@ import { FormControl, FormGroup,FormBuilder } from '@angular/forms';
 export class FormulaireDynamiqueComponent implements OnInit {
   infoForm:any;
   montab:any =[];
+  choice1:boolean = false;
   //resp:any;
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
     this.initForm();
   }
 
   initForm(){
-     this.infoForm = new FormGroup({
-      nom: new FormControl(),
-      prenom: new FormControl(),
-      adresse: new FormControl(),
-      telephone: new FormControl()
+     this.infoForm = this.fb.group({
+      nom: ['',[Validators.required,Validators.minLength(3)]],
+      prenom: ['',[Validators.required,Validators.maxLength(8)]],
+      adresse: ['',[Validators.required]],
+      telephone: ['',[Validators.required]],
+      postal: ['',[Validators.required]],
+      choice:false
     })
   }
 
   test(){
     let tab1=this.infoForm.value;
-     this.montab=this.montab.push(tab1);
+    this.montab=this.montab.push(tab1);
     console.log("voici le nom : ",this.infoForm.get('nom').value);
+    console.log("voici le nom : ",this.infoForm.value,this.infoForm.get('telephone').value);
     console.log("voici le nom1 : ",tab1['nom']);
     console.log("les valeurs que j'ai ajouter dans le tableau :",this.montab['nom']);
+  }
+  choiceEnvoi(choix:any){
+    console.log("la valeur de son choix est :",choix)
+    if(choix=='email'){
+      this.choice1=false
+    }
+    else if(choix=="boite"){
+      this.choice1=true;
+    }
+
   }
 
 }
